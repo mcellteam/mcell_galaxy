f7a17bd1af9d8cb4962283320a25d961b5432d7d
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-10-10 21:45:56 -0700

    Got a basic sine plot working with GnuPlot.

---
+++ plot_gnu.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+Plot a file ...
+  arg: /scratch/Galaxy/galaxy/tools/mcell/plot_gnu.py
+
+  arg: /scratch/Galaxy/galaxy/database/files/000/dataset_40.dat
+  arg: 1      # Column  to plot as x
+  arg: 2,3,5  # Columns to plot as y
+  arg: Plot   # Plot title
+  arg: V1     # Label for Y axis
+  arg: 0      # Y Min
+  arg: 0      # Y Max
+  arg: /scratch/Galaxy/galaxy/database/files/000/dataset_71.dat
+  arg: 800,600  # output size
+"""
+
+import string
+import sys
+import tempfile
+import subprocess
+
+assert sys.version_info[:2] >= ( 2, 4 )
+
+f = open ( '/scratch/Galaxy/galaxy/junk.txt', 'w' )
+f.write ( "In __main__\n" )
+for arg in sys.argv:
+  f.write ( "  arg: " + str(arg) + "\n" )
+
+p = subprocess.Popen ( [ "gnuplot" ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+p.stdin.write ( "set term png\n".encode() )
+# p.stdin.write ( "set output '/scratch/Galaxy/galaxy/cos88.png'\n".encode() )
+p.stdin.write ( "set output '" + sys.argv[8] + "'\n".encode() )
+p.stdin.write ( "plot cos(x)\n".encode() )
+p.stdin.write ( "quit\n".encode() )
+p.stdin.flush()
+
